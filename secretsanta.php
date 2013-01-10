282fa30525d417ff6cd8dbd5be49b6192a9702a3
Robert Norigian <bikefever@server.fake>
2013-01-10 12:57:39 -0500

    Optimizations

--- secretsanta.php
+++ secretsanta.php
@@ -1,28 +1,72 @@
 <?php
-	$names = array('person1', 'person2', 'person3', 'person4', 'person5', 'person6');
-	$emails = array('person1'=>'person1@host.com', 'person2'=>'person2@host.com', 'person3'=>'person3@host.net', 'person4'=>'person4@host.com', 'person5'=>'person5@host.com', 'person6'=>'person6@host.com');
-	santa($names, $gifter, $giftee);
-	email($gifter, $giftee, $emails);
-	echo "sent.";
-	function santa($names, &$gifter, &$giftee) {
-		shuffle($names);
-		$shuffled = $names;
-		$gifter = $names;
-		shuffle($shuffled);
-		$giftee = $shuffled;
-		$size = count($gifter);
-		$error = '';
-		for ($i = 0; $i < $size; ++$i)
-			if (($gifter[$i] == $giftee[$i]) or ($gifter[$i] == 'person1' and $giftee[$i] == 'person2' or $gifter[$i] == 'person2' and $giftee[$i] == 'person1') or ($gifter[$i] == 'person3' and $giftee[$i] == 'person4' or $gifter[$i] == 'person4' and $giftee[$i] == 'person3'))
-				$error .= 1;
-		if ($error) //re-shuffle till you get it right
-			santa($names, $gifter, $giftee);
-	}
-	function email($gifter, $giftee, $emails) {
-		$i = 0;
-		foreach ($gifter as $thisgifter) {
-			mail($emails[$thisgifter], 'Your Swiss Family Robinson secret santa recipient!', 'Secret santa says you must buy a gift for: '.$giftee[$i]."!", "From: Santa <santa@northpole>\r\n"."Reply-to: person@host.com\r\n");
-			++$i;
-		}
-	}
+    //participants
+    $names =  array(
+        'Person 1'=>'person1@host.com',
+        'Person 2'=>'person2@host.com',
+        'Person 3'=>'person3@host.com',
+        'Person 4'=>'person4@host.com',
+        'Person 5'=>'person5@host.com'
+    );
+
+    $gifter = array();
+    $giftee = array();
+
+    santa($names, $gifter, $giftee);
+    email($gifter, $giftee, $emails);
+    echo "done.";
+
+    function santa($names, &$gifter, &$giftee) {
+        uksort($names, "randCompare");
+        $shuffled = $names;
+        $gifter = $names;
+        uksort($shuffled, "randCompare");
+        $giftee = $shuffled;
+
+        $size = count($gifter);
+        $error = '';
+
+        $gifterKeys = array_keys($gifter);
+        $gifteeKeys = array_keys($giftee);
+
+        for ($i = 0; $i < $size; ++$i) {
+            if ((
+                //prevention cases
+                $gifter[$gifterKeys[$i]] == $giftee[$gifteeKeys[$i]]) or ((
+                //example; disallow this couple from being assigned each other
+                $gifter[$gifterKeys[$i]] == $names['Person 1'] and 
+                $giftee[$gifteeKeys[$i]] == $names['Person 2'] or
+                $gifter[$gifterKeys[$i]] == $names['Person 2'] and
+                $giftee[$gifteeKeys[$i]] == $names['Person 1']))
+            ) {
+                $error .= 1; //flag
+            }
+        }
+
+        if ($error) {
+            //re-shuffle till you get it right
+            santa($names, $gifter, $giftee);
+        }
+    }
+
+    function email($gifter, $giftee, $emails) {
+        $i = 0;
+        foreach ($gifter as $giftername=>$gifteremail) {
+            $gifteeKeys = array_keys($giftee);
+            echo "<p>to: $gifteremail buys for ".$gifteeKeys[$i]."</p>";
+            
+            //example mail() call:
+            /*mail(
+                $gifteremail,
+                'Your secret santa recipient!', // subject
+                'Secret santa says you must buy a gift for: '.$gifteeKeys[$i]."!", //body
+                "From: Santa <santa@northpole>\r\n"."Reply-to: replyaddress@host.com\r\n" //from
+            );*/
+
+            ++$i;
+        }
+    }
+
+    function randCompare() {
+        return rand() > rand();
+    }
 ?>
